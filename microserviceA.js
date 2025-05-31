94b910e34fb31e350d110a647954d2d2a078e5f6
Austin Flores <auflores@tesla.com>
2025-05-31 14:03:12 -0700

    Update microserviceA.js
    small update

--- microserviceA.js
+++ microserviceA.js
@@ -15,6 +15,7 @@
 // node-fetch is needed to call PokeAPI
 require ('dotenv').config();
 const express   = require("express");
+const cors = require('cors');
 const http      = require("http");
 const WebSocket = require('ws');
 const fetch     = require('node-fetch');
@@ -26,6 +27,7 @@ const HOST = process.env.HOST || "localhost";
 const PORT = parseInt(process.env.PORT, 10) || 5050;
 
 // Middleware:
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 //confirm backend is working
